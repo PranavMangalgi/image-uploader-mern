59172b601690ddf97d6466faef53a4514cec3bdb
pranavmangalgi <pranavmangalgi@gmail.com>
2024-01-05 15:50:47 +0530

    vercel config

--- server/routes/imageOps.js
+++ server/routes/imageOps.js
@@ -4,6 +4,10 @@ const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 const { handleUpload, handleUrl } = require("../controllers/imageFuncs");
 
+router.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 router.post("/upload", upload.single("img"), handleUpload);
 
 router.get("/:shortUrl", handleUrl);
